6b9b9b1fb89d67f9bf2b1c9ff6aa0619663bf109
BadTripp <renato.turetta00@gmail.com>
2021-09-28 16:09:38 +0200

    Fix logo- fix css homeCard(update)

--- src/Components/Layout/Homecard.js
+++ src/Components/Layout/Homecard.js
@@ -6,14 +6,14 @@ import classes from "./Homecard.module.css";
 const HomeCard = (props) => {
   return (
     <div className={classes.box}>
-    <Card style={{ width: "25rem"}}>
-      <Card.Img variant="top" src={logoImg} />
+    <Card style={{ width: "25rem"}} className={classes.card}>
+      <Card.Img  className={classes.card}variant="top" src="https://uploads.gamedev.net/gallery/monthly_2021_01/0ee4d4d3d3be4551b2a2688b8cb47b60.GL-001.gif" />
       <Card.Body>
         <Card.Title>Update 0.1</Card.Title>
         <Card.Text>
         View the changelog of the latest update
         </Card.Text>
-        <Button variant="primary">Visualizza</Button>
+        <Button variant="primary">View</Button>
       </Card.Body>
     </Card>
     </div>
